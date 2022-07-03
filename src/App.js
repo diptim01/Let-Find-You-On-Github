72bfb9f826c7c6527c1de4f6c9ba83ca18e322c0
Oladipo.Alokan <oladipo.alokan@globalaccelerex.com>
2022-07-03 23:46:08 +0100

    setup tailwind and daisy UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <div className="bg-purple-500">
-        <h1 className="text-xl">Hello World!</h1>
-      </div>
+    <div>
+     <h1 >Hello world</h1>
     </div>
   );
 }
