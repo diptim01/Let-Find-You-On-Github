530345da1424bbc25137b27fe7ad0bf6c864a040
Oladipo.Alokan <oladipo.alokan@globalaccelerex.com>
2022-07-13 22:21:00 +0100

    clean up

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -6,14 +6,15 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-   const initialState = {
+  const initialState = {
     users: [],
-    loading: true
-   }
+    loading: false,
+  };
 
-   const [state, dispatch] = useReducer(githubReducer, initialState)
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -21,19 +22,24 @@ export const GithubProvider = ({ children }) => {
     });
 
     const data = await response.json();
-  
+
     dispatch({
-        type: "GET_USERS",
-        payload: data
-    })
+      type: "GET_USERS",
+      payload: data,
+    });
   };
 
+  const setLoading = () =>
+    dispatch({
+      type: "SET_LOADING",
+    });
+    
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers
+        fetchUsers,
       }}
     >
       {children}
