c824074f736cf076b143b3a6bb742fc5b9921001
Oladipo.Alokan <oladipo.alokan@globalaccelerex.com>
2022-07-13 22:51:52 +0100

    user search component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import UserResults from "../components/users/UserResults"
+import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
 
 function Home() {
   return (
     <>
-    {/* search component */}
-     <UserResults />
+      {/* search component */}
+      <UserSearch />
+      <UserResults />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
