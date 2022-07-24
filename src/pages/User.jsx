c820bde7d82584d4db3216d2be43c7deaf221317
Oladipo.Alokan <oladipotimothy69@gmail.com>
2022-07-24 15:13:57 +0100

    added axios & fixed the scrollbar for small device

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -11,7 +11,7 @@ import Spinner from "../components/layout/Spinner";
 import { useParams } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
 import RepoList from "../components/repos/RepoList";
-import { getUser, getUserRepos } from "../context/github/GithubActions";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function User() {
   const { user, loading, repos, dispatch } = useContext(GithubContext);
@@ -20,11 +20,8 @@ function User() {
   useEffect(() => {
     dispatch({ type: "SET_LOADING" });
     const getUserData = async () => {
-      const userData = await getUser(params.login);
-      dispatch({ type: "GET_USER" , payload: userData });
-
-      const userRepoData = await getUserRepos(params.login);
-      dispatch({ type: "GET_REPOS" , payload: userRepoData });
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
     };
 
     getUserData();
@@ -135,57 +132,58 @@ function User() {
           </div>
         </div>
         <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaUsers className="text-3xl md:text-5xl" />
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUsers className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Followers</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {followers}
+              </div>
             </div>
-            <div className="stat-title pr-5">Followers</div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
-              {followers}
-            </div>
-          </div>
 
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaUserFriends className="text-3xl md:text-5xl" />
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUserFriends className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Following</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {following}
+              </div>
             </div>
-            <div className="stat-title pr-5">Following</div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
-              {following}
-            </div>
-          </div>
 
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaCodepen className="text-3xl md:text-5xl" />
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaCodepen className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Repos</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {public_repos}
+              </div>
             </div>
-            <div className="stat-title pr-5">Public Repos</div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
-              {public_repos}
-            </div>
-          </div>
 
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaLaptopCode className="text-3xl md:text-5xl" />
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaLaptopCode className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Private Repos</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {total_private_repos == null ? 0 : total_private_repos}
+              </div>
             </div>
-            <div className="stat-title pr-5">Private Repos</div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
-              {total_private_repos == null ? 0 : total_private_repos}
-            </div>
-          </div>
 
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaStore className="text-3xl md:text-5xl" />
-            </div>
-            <div className="stat-title pr-5">Public Gists</div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
-              {public_gists}
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaStore className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Gists</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {public_gists}
+              </div>
             </div>
           </div>
         </div>
-
         <RepoList repos={repos} />
       </div>
     </>
